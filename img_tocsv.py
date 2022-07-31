d2ef79642cdd6377e371ffe5713d269f99f05920
dragonfruits-me <97835956+dragonfruits-me@users.noreply.github.com>
2022-07-31 17:08:07 +0900

    Update img_tocsv.py

--- img_tocsv.py
+++ img_tocsv.py
@@ -1,62 +1,27 @@
+#インポート
+import os
 from time import sleep
-from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+
 import pandas as pd
+import requests
 
+#画像格納先フォルダ
+IMAGE_DIR = "./img/"
 
-chrome_path = r"C:\Users\Takekawa Takumi\2022725hayatasu\chromedriver.exe"
+#csvファイルの読み込み
+df = pd.read_csv("image_urls_dragonfruits.csv")
 
+#画像格納先フォルダ（img）が存在するかどうかの処理。存在しない場合、新たに作成。
+if os.path.isdir(IMAGE_DIR):
+    print("既に存在します。")
+else:
+    os.makedirs(IMAGE_DIR)
 
-driver = webdriver.Chrome(chrome_path)
+#画像の保存
+for filename, yahoo_img_url in zip(df.filename, df.yahoo_img_url):
+   image = requests.get(yahoo_img_url)
+   with open(IMAGE_DIR + filename + ".jpg", "wb") as f:
+        f.write(image.content)
+   
+   sleep(2)
 
-url ="https://search.yahoo.co.jp/image"
-driver.get(url)
-
-sleep(3)
-
-query="プログラミング"
-
-# search_box = driver.find_element_by_class_name("SearchBox__searchInput")
-# search_box = driver.find_element_by_class("SearchBox__searchInput")
-search_box = driver.find_element(By.CLASS_NAME,"SearchBox__searchInput")
-search_box.send_keys(query)
-search_box.submit()
-
-
-
-sleep(3)
-height = 1000
-
-
-while height < 3000:
-    driver.execute_script("window.scrollTo(0,{});".format(height))
-    height += 100
-    sleep(1)
-
-elements = driver.find_elements(By.CLASS_NAME,"sw-Thumbnail--tile")
-
-d_list = []
-
-for i, element in enumerate(elements, start=1):
-    name=f"{query}_{i}"
-    yahoo_img_url = element.find_element(By.TAG_NAME,"img").get_attribute("src")
-    raw_url = element.find_element(By.CLASS_NAME,"sw-ThumbnailGrid__details").get_attribute("href")
-    title = element.find_element(By.TAG_NAME,"img").get_attribute("alt") 
-
-    d={
-        "filenqame": name,
-        "raw_url": raw_url,
-        "yahoo_img_url": yahoo_img_url,
-        "title": title
-    }
-
-    d_list.append(d)
-    sleep(2)
-
-
-df = pd.DataFrame(d_list)
-df.to_csv("image_urls_2018.csv")
-
-driver.quit()
